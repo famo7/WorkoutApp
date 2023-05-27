ea87a0971ce9b55b97f0277b9bdf19060555efe0
famo1901 <famo1901@gmail.com>
2023-05-27 22:49:14 +0200

    deploy

--- WorkoutApp/Program.cs
+++ WorkoutApp/Program.cs
@@ -55,20 +55,6 @@ if (builder.Environment.IsDevelopment())
 else
 {
     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
-    //var updatedHost = pgHost.Replace("fly.dev", "internal");
-
-    connString = $"Server ={"strengthseeker-db.internal"}; Port ={pgPort}; User Id = {pgUser}; Password ={pgPass}; Database ={pgDb};";
 }
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
 {
